76b97240c8b1d0467a0e255b466b7ea2f67a2353
dir-FY24shimizukaito <kaito.shimizu.u257@dirbato.co.jp>
2024-04-12 15:57:02 +0900

    q

---
+++ Practice/src/fight/CalculatorServiceTest.java
@@ -0,0 +1,30 @@
+package fight;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CalculatorServiceTest {
+    
+    private Calculator calculator;
+    private CalculatorService service;
+    
+    @BeforeEach
+    public void setUp() {
+        calculator = new Calculator();
+        service = new CalculatorService(calculator);
+    }
+
+    @Test
+    public void testAddAndReport() {
+        assertEquals("Result: 5", service.addAndReport(2,  3));
+        
+    }
+    
+    @Test
+     testSubtractAndReport() {
+        assertEquals("Result: 1", service.subtractAndReport(3,  2));
+    }
+
+}
---
+++ Practice/src/fight/four.java
@@ -0,0 +1,14 @@
+package fight;
+
+import javax.imageio.ImageTranscoder;
+
+public class four {
+    public static void main(String[] args) {
+        System.out.println( "2と4の平均=" + average( 2,4 ));
+    }
+    static int average( int x, int y ) {
+        return( x + y );
+    }
+        
+                
+    }
